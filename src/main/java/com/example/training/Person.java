f7094a86907c3cba4aa9629b2de11f070f0c2c7c
Samuel Quintero Agudelo <squinteroagudelo18@gmail.com>
2022-04-06 01:49:32 -0500

    Springboot MVC, Thymeleaf...

---
+++ src/main/java/com/example/training/Person.java
@@ -0,0 +1,11 @@
+package com.example.training;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private String lastname;
+    private String email;
+    private String phone;
+}
